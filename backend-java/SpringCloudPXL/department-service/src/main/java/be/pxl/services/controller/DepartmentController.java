4d1d3c86f60bb327a998ec69b1d324c24cef9650
Mike van Bree <mike.vanbree@student.pxl.be>
2024-09-25 20:54:17 +0200

    Added Dependency Injection

--- backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/controller/DepartmentController.java
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package be.pxl.services.controller;
 import be.pxl.services.domain.dto.DepartmentRequest;
 import be.pxl.services.domain.dto.DepartmentResponse;
 import be.pxl.services.service.IDepartmentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,8 +11,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/department")
+@RequiredArgsConstructor
 public class DepartmentController {
-    private IDepartmentService departmentService;
+    private final IDepartmentService departmentService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
