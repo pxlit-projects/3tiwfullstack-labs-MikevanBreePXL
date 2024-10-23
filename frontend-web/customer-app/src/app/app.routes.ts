5049f2344404e54f7643319b5fc3e986de24b16c
MikevanBreePXL <mike.vanbree@student.pxl.be>
2024-10-23 22:10:13 +0200

    [FRONTEND] Lab 7 Finished

--- frontend-web/customer-app/src/app/app.routes.ts
+++ frontend-web/customer-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './core/customers/customer-list/customer-list.component';
+import { AddCustomerComponent } from './core/customers/add-customer/add-customer.component';
+import { CustomerDetailComponent } from './core/customers/customer-detail/customer-detail.component';
+import { confirmLeaveGuard } from './confirm-leave.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'customers', component: CustomerListComponent},
+    {path: 'add', component: AddCustomerComponent},
+    {path: 'customer/:id', component: CustomerDetailComponent},
+    {path: 'add', component: AddCustomerComponent, canDeactivate: [confirmLeaveGuard]},
+    {path: '', redirectTo: 'customers', pathMatch: 'full'} // Checks for empty URL (pathMatch: 'full' means no regex, full ONLY)
+];
--- frontend-web/customer-app/src/app/core/customers/add-customer/add-customer.component.ts
+++ frontend-web/customer-app/src/app/core/customers/add-customer/add-customer.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, inject, Output} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Customer} from "../../../shared/models/customer.model";
+import { Router } from '@angular/router';
+import { CustomerService } from '../../../shared/services/customer.service';
 
 @Component({
   selector: 'app-add-customer',
@@ -10,7 +12,11 @@ import {Customer} from "../../../shared/models/customer.model";
   styleUrl: './add-customer.component.css'
 })
 export class AddCustomerComponent {
+
+  customerService: CustomerService = inject(CustomerService);
+  router: Router = inject(Router);
   fb: FormBuilder = inject(FormBuilder);
+  
   customerForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
@@ -20,12 +26,14 @@ export class AddCustomerComponent {
     country: ['', Validators.required],
     vat: ['', [Validators.required, Validators.min(0)]]
   });
-  @Output() addCustomer = new EventEmitter<Customer>();
 
   onSubmit() {
     const newCustomer: Customer = {
       ...this.customerForm.value
     };
-    this.addCustomer.emit(newCustomer);
+    this.customerService.addCustomer(newCustomer).subscribe(customer => {
+      this.customerForm.reset();
+      this.router.navigate(['/customers']);
+    });
   }
 }
